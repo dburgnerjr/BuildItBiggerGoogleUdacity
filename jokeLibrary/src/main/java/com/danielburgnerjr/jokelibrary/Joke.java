8a296fa785c1d6df124e34e4dd4d03ac6765476c
Daniel Burgner <dburgnerjr@yahoo.com>
2018-08-06 20:47:26 -0500

    Build It Bigger Day 2.
    Added Android library.  #GrowWithGoogle #AndroidDev
    #GoogleUdacityScholars

--- jokeLibrary/src/main/java/com/danielburgnerjr/jokelibrary/Joke.java
+++ jokeLibrary/src/main/java/com/danielburgnerjr/jokelibrary/Joke.java
@@ -13,8 +13,8 @@ public class Joke {
         this.jokeList.add("When, you're shopping with the kids, the three-year-old asks for something and the seven-year-old tells him, \"It's not in the budget.\"");
         this.jokeList.add("When you're reading this in your father-in-law’s driveway because you have to glom off of his wi-fi.");
         this.jokeList.add("When you use a coupon for a free hamburger from Mc Donald's and then come home and put your own piece of cheese on it!");
-        this.jokeList.add("Software engineer (noun.)\nA machine that turns pizza into code");
-        this.jokeList.add("Lottery (noun.)\nA tax on people who are bad at math");
+        this.jokeList.add("Software engineer (n.)\nA machine that turns pizza into code");
+        this.jokeList.add("Lottery (n.)\nA tax on people who are bad at math");
     }
 
     public String getJoke() {
