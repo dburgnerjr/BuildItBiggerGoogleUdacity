aad6616acc673fa7fb9d5b721b28f36d77e79f79
Daniel Burgner <dburgnerjr@yahoo.com>
2018-08-09 07:24:34 -0500

    Build It Bigger Day 3.
    Added unit testing.  #GrowWithGoogle #AndroidDev #GoogleUdacityScholars

---
+++ app/src/androidTest/java/com/danielburgnerjr/builditbiggergoogleudacity/AsyncJokeTaskTest.java
@@ -0,0 +1,40 @@
+package com.danielburgnerjr.builditbiggergoogleudacity;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class AsyncJokeTaskTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void jokeAsyncTaskTest() {
+        onView(withId(R.id.tell_joke_button))
+                .check(matches(withText(R.string.button_text)));
+
+        String result = null;
+        AsyncJokeTask asyncJokeTask = new AsyncJokeTask();
+        asyncJokeTask.execute();
+        try {
+            result = asyncJokeTask.get();
+            Log.d("testing", "Retrieved a non-empty string successfully: " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertNotNull(result);
+    }
+
+}
